4348746baf3d4152a1274a7e34d2213eebba2c8c
Suiwu <48559707+Suiwu@users.noreply.github.com>
2021-12-22 13:29:18 +0800

    new

--- doc/intro.R
+++ doc/intro.R
@@ -1,4 +1,43 @@
 ## ----eval=FALSE---------------------------------------------------------------
+#  function (age, female, ily)
+#  {
+#      p <- 0.25 + 0.3 * 1/(1 - exp(0.04 * age)) + 0.1 * ily
+#      p <- p * ifelse(female, 1.25, 0.75)
+#      p <- pmax(0, p)
+#      p <- pmin(1, p)
+#      p
+#  }
+
+## ----eval=FALSE---------------------------------------------------------------
+#  double vacc3a(double age, bool female, bool ily){
+#    double p = 0.25 + 0.3 * 1 / (1 - exp(0.04 * age)) + 0.1 * ily;
+#    p = p * (female ? 1.25 : 0.75);
+#    p = std::max(p, 0.0);
+#    p = std::min(p, 1.0);
+#    return p;
+#  }
+#  NumericVector vaccC(NumericVector age, LogicalVector female,
+#                      LogicalVector ily) {
+#    int n = age.size();
+#    NumericVector out(n);
+#    for(int i = 0; i < n; ++i) {
+#      out[i] = vacc3a(age[i], female[i], ily[i]);
+#    }
+#    return out;
+#  }
+
+## ----eval=TRUE----------------------------------------------------------------
+library(StatComp21055)
+library(microbenchmark)
+data(data)
+attach(data)
+tm2 <- microbenchmark(
+  vR = vaccR(age,female,ily),
+  vC = vaccC(age,female,ily)
+)
+knitr::kable(summary(tm2)[,c(1,3,5,6)])
+
+## ----eval=FALSE---------------------------------------------------------------
 #  gibbsR <- function(N, thin) {
 #    mat <- matrix(nrow = N, ncol = 2)
 #    x <- y <- 0
@@ -26,4 +65,11 @@
 #    }
 #    return(mat);
 #  }
+
+## ----eval=TRUE----------------------------------------------------------------
+tm2 <- microbenchmark(
+  vR = gibbsR(1e4, 10),
+  vC = gibbsC(1e4, 10)
+)
+knitr::kable(summary(tm2)[,c(1,3,5,6)])
 
